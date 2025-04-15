e8874a4332378efcc39d72a8d9e6ea0f2db08502
edwardcarlosravn <edwardcarlos@ravn.co>
2025-04-15 17:21:31 -0500

    feat: find-majority-element solved

--- 1-js-and-async-programming/1-javascript-fundamentals/5-find-majority-element.js
+++ 1-js-and-async-programming/1-javascript-fundamentals/5-find-majority-element.js
@@ -19,6 +19,23 @@ findMajorityElement([1, 2, 3, 4, 5]); // Expected output: null
 
 */
 
-const findMajorityElement = (arr) => {};
+const findMajorityElement = (arr) => {
+    if(arr.length < 1) return null;
+    if(arr.length === 1) return arr[0];
+    let candidate = null;
+    let count = 0; 
+    for(num of arr){
+        if(count === 0){
+            candidate = num;
+            count = 1;
+        } else if(candidate === num){
+            count++
+        }else{
+            count--;
+        }
+    }
+    count = arr.filter(num => num === candidate).length;
+    return (count > arr.length/2) ? candidate : null;
+};
 
 module.exports = findMajorityElement;
