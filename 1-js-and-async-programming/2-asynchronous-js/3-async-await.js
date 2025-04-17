ba402b3a1ffc1ab0292b5bf45aaf8cf2e6baadd0
edwardcarlosravn <edwardcarlos@ravn.co>
2025-04-17 15:24:24 -0500

    refactor: create getReviewerSubscriptions for better code readability

--- 1-js-and-async-programming/2-asynchronous-js/3-async-await.js
+++ 1-js-and-async-programming/2-asynchronous-js/3-async-await.js
@@ -22,19 +22,24 @@ const getMovieCount = (movieList, userId) => {
   const userMovies = movieList.find(item => item.userId === userId);
   return userMovies ? userMovies.movies.length : 0;
 }
+const getReviewerSubscriptions = async (users, likedMovies, dislikedMovies) => {
+    const reviewerUsers = await Promise.all(
+      users.map(async user => {
+        const likesCount = getMovieCount(likedMovies, user.id);
+        const dislikesCount = getMovieCount(dislikedMovies, user.id);
+        if (likesCount < dislikesCount) {
+          const subscription = await getUserSubscriptionByUserId(user.id);
+          return subscription?.subscription || null;
+        }
+        return null;
+      })
+    )
+    return reviewerUsers.filter(subscription => subscription !== null)
+}
 const getCommonDislikedSubscription = async () => {
   try {
     const [users, likedMovies, dislikedMovies] = await Promise.all([getUsers(), getLikedMovies(),getDislikedMovies()]);
-    const processedUsers = users.map(async user => {
-      const likesCount = getMovieCount(likedMovies, user.id);
-      const dislikesCount = getMovieCount(dislikedMovies, user.id);
-      if (likesCount < dislikesCount) {
-        const subscription = await getUserSubscriptionByUserId(user.id);
-        return subscription?.subscription || '';
-      }
-      return null;
-    });
-    const subscriptions = (await Promise.all(processedUsers)).filter(subscription => subscription !== null && subscription !== '');
+    const subscriptions = await getReviewerSubscriptions(users, likedMovies, dislikedMovies);
     if(subscriptions.length === 0) return "No subscription for people with more dislikes than likes";
     const subscriptionCounts = subscriptions.reduce((acc, sub) => {
       acc[sub] = (acc[sub] || 0) + 1;
@@ -42,7 +47,9 @@ const getCommonDislikedSubscription = async () => {
     },{})
     return Object.entries(subscriptionCounts).sort((a, b) => b[1] - a[1])[0][0];
   } catch (err) {
-    throw err;
+    console.log('Failed to determine subscription:', err.message);
+    return "Error processing data";
+    // throw new Error(err);
   }
 };
 
