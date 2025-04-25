7fab24a8541709e1abaf045c8aa28692aecf2cdd
edwardcarlosravn <edwardcarlos@ravn.co>
2025-04-25 18:21:58 -0500

    feat: deep clone function created

--- 3-typescript/2-custom-utility-types-and-generics/1-deep-clone.ts
+++ 3-typescript/2-custom-utility-types-and-generics/1-deep-clone.ts
@@ -12,3 +12,34 @@
  */
 
 //? implement the function  here
+export function deepClone<T>(value: T): T {
+    if(value === null || value === undefined) return value;
+    if(typeof value !== 'object') return value;
+    if(value instanceof Date) return new Date(value.getTime()) as T;
+    if(value instanceof RegExp) return new RegExp(value.source, value.flags) as T;
+    if(Array.isArray(value)){
+        return value.map(item => deepClone(item)) as T;
+    }
+    if(value instanceof Map){
+        const clonedMap = new Map();
+        value.forEach((val, key) => {
+            clonedMap.set(deepClone(key), deepClone(val));
+        })
+        return clonedMap as T;
+    }
+    if(value instanceof Set){
+        const clonedSet = new Set();
+        value.forEach(val => {
+            clonedSet.add(deepClone(val));
+        })
+        return clonedSet as T;
+    }
+    const cloneObject = {} as T;
+    for(const key in value){
+        if(Object.prototype.hasOwnProperty.call(value,key)){
+            cloneObject[key] = deepClone(value[key]);
+        }
+    }
+    return cloneObject
+
+}
